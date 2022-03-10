95071b71ce5150bf2f0142ef1ac39d4ff97cce94
matsblock <96005274+matsblock@users.noreply.github.com>
2022-03-10 13:58:01 -0300

    Update enetscoreInterfaces.sol

--- enetscoreInterfaces.sol
+++ enetscoreInterfaces.sol
@@ -11,30 +11,34 @@ interface IEnetscore {
 }
 
 contract enetscoreInterfaces {
-      constructor () {
+
+    address public enetscore_address;
+
+    constructor () {
+      enetscore_address = 0x3C36cD60da07C7792C00D20a28274A451e47cb32;
     }
 
-    function scheduleAsString(address _address) external view returns (string memory) {
-        return IEnetscore(_address).scheduleAsString();
+    function scheduleAsString() external view returns (string memory) {
+        return IEnetscore(enetscore_address).scheduleAsString();
     }
 
-    function gameDetailsAsString(address _address) external view returns (string memory) {
-        return IEnetscore(_address).gameDetailsAsString();
+    function gameDetailsAsString() external view returns (string memory) {
+        return IEnetscore(enetscore_address).gameDetailsAsString();
     }
 
-    function callGameScoreAsString(address _address) external view returns (string memory) {
-        return IEnetscore(_address).gameScoreAsString();
+    function callGameScoreAsString() external view returns (string memory) {
+        return IEnetscore(enetscore_address).gameScoreAsString();
     }
 
-    function requestSchedule(address _address, uint256 _leagueId, string memory _date) external{
-        return IEnetscore(_address).requestSchedule(_leagueId, _date);
+    function requestSchedule(uint256 _leagueId, string memory _date) external{
+        return IEnetscore(enetscore_address).requestSchedule(_leagueId, _date);
     }
 
-    function requestGameDetails(address _address, uint256 _gameId) external{
-        return IEnetscore(_address).requestGameDetails(_gameId);
+    function requestGameDetails(uint256 _gameId) external{
+        return IEnetscore(enetscore_address).requestGameDetails(_gameId);
     }
 
-    function requestGameScore(address _address, uint256 _gameId) external{
-        return IEnetscore(_address).requestGameScore(_gameId);
+    function requestGameScore(uint256 _gameId) external{
+        return IEnetscore(enetscore_address).requestGameScore(_gameId);
     }
 }
