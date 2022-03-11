ce970d1521eb027eb310c7f32407c0112b36b93a
matsblock <96005274+matsblock@users.noreply.github.com>
2022-03-11 12:23:21 -0300

    Update enetscoreInterfaces.sol

--- enetscoreInterfaces.sol
+++ enetscoreInterfaces.sol
@@ -15,7 +15,7 @@ contract enetscoreInterfaces {
     address public enetscore_address;
 
     constructor () {
-      enetscore_address = 0x6402B63BC71B8833a8345e410B415DA4C7647Fe7;
+      enetscore_address = 0x3C36cD60da07C7792C00D20a28274A451e47cb32;
     }
 
     function scheduleAsString() external view returns (string memory) {
@@ -31,14 +31,15 @@ contract enetscoreInterfaces {
     }
 
     function requestSchedule(uint256 _leagueId, string memory _date) external{
-        return IEnetscore(enetscore_address).requestSchedule(_leagueId, _date);
+        IEnetscore(enetscore_address).requestSchedule(_leagueId, _date);
     }
 
     function requestGameDetails(uint256 _gameId) external{
-        return IEnetscore(enetscore_address).requestGameDetails(_gameId);
+        IEnetscore(enetscore_address).requestGameDetails(_gameId);
     }
 
     function requestGameScore(uint256 _gameId) external{
-        return IEnetscore(enetscore_address).requestGameScore(_gameId);
-    }
+        IEnetscore(enetscore_address).requestGameScore(_gameId);
+    }     
 }
+
